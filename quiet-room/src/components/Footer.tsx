976fc0492e756c449a6921e4f80ebf527a45f5a8
Salwa Jeries <annette.jeries@gmail.com>
2023-07-25 13:31:29 -0500

    Added Github source code and Feedback form links in footer

--- quiet-room/src/components/Footer.tsx
+++ quiet-room/src/components/Footer.tsx
@@ -1,10 +1,15 @@
-import { Typography } from '@mui/material'
+import { Chip, Divider, IconButton, Typography } from '@mui/material'
+import Link from 'next/link'
 import React from 'react'
 
 export default function Footer() {
 
+    const handleGithub = (e: any) => {
+        console.log('Handle Github click')
+    }
+
     return (
-        <div style={{ 
+        <div id='#bottom' style={{ 
             width: '100vw',
             height: '12vh',
             backgroundColor: '#CFC9C9',
@@ -20,6 +25,24 @@ export default function Footer() {
                 <li><Typography variant='body1' width={'100%'} flexBasis={'100%'}>UAH QuietRoom</Typography></li>
                 <li><Typography variant='body1' width={'100%'}>Copyright © 2023</Typography></li>
             </ul>
+            <Divider variant='middle'></Divider>
+            <ul style={{ listStyleType: 'none', margin: '0', padding: '0', height: 'auto' }}>
+                <li>
+                    <Link href="https://forms.gle/PamUMtey2cBjPAnD6">
+                        <Chip variant='outlined' label='Feedback' onClick={handleGithub}
+                            avatar={<img src="comment-regular.svg" height="30px" alt="Feedback" />}
+                        />
+                    </Link>
+                </li>
+                <li>
+                    <Link href="https://github.com/QuietRoomUAH/Quiet-Room-Frontend">
+                        <Chip variant='outlined' label='Github' onClick={handleGithub}
+                            avatar={<img src="github-icon.svg" height="30px" alt="Github" />}
+                        />
+                    </Link>
+                </li>
+            </ul>
+            
         </div>
     )
 }
